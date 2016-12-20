3e85b8a34611d5052025e40a4f407c4573411ebe
Delvin <delvin.fil@gmail.com>
2016-12-20 14:27:27 +0800

    Add files via upload

--- header.php
+++ header.php
@@ -2,11 +2,13 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="Description" content="From Delvin LabDesign &copy. "> 
+<meta name="Keywords" content="Прогноз погоды и многое другое "> 
 <link rel="stylesheet" type="text/css" href="my.css" />
 </head>
 <body>
-<div  id="header">
-<h3> Lighttp+PHP+Python </h3>
+<div  id="content-head">
+	<h3> Lighttp+PHP+Pýthon </h3>
 </div>
 </body>
 </html>
