331f095a52c142308c3bfdc899a1dc7cb6f23e46
delvin-fil <delvin.fil@gmail.com>
2016-12-20 17:19:24 +0700

    new commit

---
+++ pages/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="Description" content="From Delvin LabDesign &copy. "> 
+<meta name="Keywords" content="Прогноз погоды и многое другое "> 
+<link rel="stylesheet" type="text/css" href="my.css" />
+</head>
+<body>
+<div  id="content-head">
+	<h3> Lighttp+PHP+Pýthon </h3>
+</div>
+</body>
+</html>
